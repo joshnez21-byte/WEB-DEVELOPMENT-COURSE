1ebfbd30ad14d79888234c395d84b16ba223ce0c
joshnez21-byte <joshnez21@gmail.com>
2026-01-25 18:39:10 +0300

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,12 @@
+#Printing a greeting message
+print("Hello!")
+
+#Printing my name
+print("My name is Joshua")
+
+#Printing a fun fact about myself
+#print("A fun fact about me is that I love Basketball")
+print("A fun fact about me is that I love Basketball.")
+
+#Performing and printing simple calculations
+print("5 + 3 =", 5 + 3)
