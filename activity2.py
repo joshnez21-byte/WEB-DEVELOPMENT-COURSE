dca2f46884f71a000081894e367ec8693b451344
joshnez21-byte <joshnez21@gmail.com>
2026-01-25 18:46:28 +0300

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,13 @@
+#Print a sentence
+print("I am the best basketballer in the world.")
+
+#Print a number
+print(3)
+
+#Next line after print
+print("Hello Python")
+
+#Print all the keywords present in Python
+print("The Keywords in Python are...")
+import keyword
+print(keyword.kwlist)
